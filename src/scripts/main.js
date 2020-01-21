7f34f50d23511b20f3bd0d506f9fbe4e10afca53
zaxwebs <zifidigital@gmail.com>
2020-01-22 04:46:43 +0530

    Added projects section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,14 +3,28 @@ import 'bootstrap'
 import Swiper from 'swiper'
 import AOS from 'aos'
 
-const coverSwiper = new Swiper('.swiper-container', {
-	// Optional parameters
+const coverSwiper = new Swiper('.swiper-container#cover-swiper', {
 	loop: true,
 	grabCursor: true,
 	parallax: true,
 	speed: 500,
 	autoplay: {
 		delay: 3000
+	}
+})
+
+const projectsSwiper = new Swiper('.swiper-container#projects-swiper', {
+	loop: true,
+	grabCursor: true,
+	slidesPerView: 1,
+	spaceBetween: 100,
+	autoplay: {
+		delay: 4000
+	},
+	breakpoints: {
+		700: {
+			slidesPerView: 3
+		}
 	}
 })
 
