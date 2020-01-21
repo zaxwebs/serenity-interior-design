a819aa2534d11c9be41af5b1ccdea58fba79572e
zaxwebs <zifidigital@gmail.com>
2020-01-21 22:16:07 +0530

    Added cover

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,14 @@
 import $ from 'jquery'
 import 'bootstrap'
+import Swiper from 'swiper'
+
+const mySwiper = new Swiper('.swiper-container', {
+	// Optional parameters
+	loop: true,
+	grabCursor: true,
+	parallax: true,
+	speed: 500,
+	autoplay: {
+		delay: 3000
+	}
+})
