7822cc6ecdccac3af6be756222b0186b10b5faa9
zaxwebs <zifidigital@gmail.com>
2020-01-22 19:51:54 +0530

    Added dynamic navbar

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -39,3 +39,13 @@ const projectsSwiper = new Swiper('.swiper-container#projects-swiper', {
 })
 
 AOS.init()
+
+$(function() {
+	$(window).on('scroll', function() {
+		if ($(window).scrollTop() > 10) {
+			$('.navbar').addClass('active')
+		} else {
+			$('.navbar').removeClass('active')
+		}
+	})
+})
